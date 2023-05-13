fa36595db1fe188c57507c10eb3f4e9c1ac341ef
anweinnnn22 <133495577+anweinnnn22@users.noreply.github.com>
2023-05-13 19:08:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'c72dee65893cdecf79f07d4e81a65f6b',
 
-  PROVINCE: '江苏',
-  CITY: '苏州',
+  PROVINCE: '广东',
+  CITY: '惠州',
 
   USERS: [
     {
