b8bf98668aea5a2c98531b440cbc7474465dead6
anweinnnn22 <133495577+anweinnnn22@users.noreply.github.com>
2023-05-13 17:54:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx709711e0e3c910db',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'c72dee65893cdecf79f07d4e81a65f6b',
 
   PROVINCE: '广东',
   CITY: '惠州',
@@ -19,17 +19,17 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小王同学',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ojef659Y-uHn_AcWIKeUQBMRLKBQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'kt3hLsv58vTJ4b1IZXIf4SgI4x0nI8L9XjVNy_2EtAY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '小王同学', year: '2004', date: '05-14',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'wMiZ7M8eQ5EgyOvMRm-bjMIVfIOJhLHB-Og6NEkWEac',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '安炜',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ojef6531L5oYl952HCXyzCovkTfg',
     }
   ],
 
